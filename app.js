5fafbc6a05e1e385b36658f2552f180c22cc7c39
Shivang Chaturvedi <shiva.chaturvedi2191@outlook.com>
2020-06-23 11:07:47 +0530

    IRN fetch Api

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const axios = require('./axios.js');
 const express = require('express');
 const app = express();
 var random = require("random-key");
-const { encryptStringWithRsaPublicKey, aesDecryption, aesEncryption, aesDataDecryption } = require('./encrypt.js')
+const encrypt = require('./encrypt.js')
 const crypto = require('crypto');
 app.use(cors());
 app.use(express.json());
@@ -33,9 +33,9 @@ app.post('/api/auth', async (req, res) => {
     try {
         let oData = req.body;
         let sPublicKeyPath = !oData.public_key ? "./public_key_dev/einv_sandbox.pem" : null
-        let sEncryptedPwd = encryptStringWithRsaPublicKey(oData.Password, oData.public_key, sPublicKeyPath);
+        let sEncryptedPwd = encrypt.encryptStringWithRsaPublicKey(oData.Password, oData.public_key, sPublicKeyPath);
         let sAppKey = crypto.randomBytes(32);
-        let sEncryptedAppKey = encryptStringWithRsaPublicKey(sAppKey, oData.public_key, sPublicKeyPath);
+        let sEncryptedAppKey = encrypt.encryptStringWithRsaPublicKey(sAppKey, oData.public_key, sPublicKeyPath);
         let oHeaders = {
             "client_id": oData.client_id,
             "client_secret": oData.client_secret
@@ -50,7 +50,7 @@ app.post('/api/auth', async (req, res) => {
             }
         }
         let { data } = await axios.post("/gstvital/v1.02/auth", oPayload, { headers: oHeaders });
-        data.Data.Sek = aesDecryption(data.Data.Sek, sAppKey);
+        data.Data.Sek = encrypt.aesDecryption(data.Data.Sek, sAppKey);
         res.status(200).json({
             Status: "Success",
             Response: data,
@@ -71,7 +71,7 @@ app.post('/api/irn/create', async (req, res) => {
         for (let i = 0; i < oData.length; i++) {
             let oItem = oData[i];
             let oResponse = {};
-            let sEncryptedData = aesEncryption(oItem, oHeader.sek)
+            let sEncryptedData = encrypt.aesEncryption(oItem, oHeader.sek)
             oResponse.doc_no = oItem.DocDtls.No;
             let oHeaders = {
                 "client_id": oHeader.client_id,
@@ -85,17 +85,53 @@ app.post('/api/irn/create', async (req, res) => {
             }
             let { data } = await axios.post("/gstcore/v1.02/Invoice", oPayload, { headers: oHeaders });
             oResponse.res = data;
-            if(!data.ErrorDetails){
-                oDecryptedData = aesDataDecryption(data.Data, oHeader.sek);
+            if (!data.ErrorDetails) {
+                oDecryptedData = encrypt.aesDataDecryption(data.Data, oHeader.sek);
                 oResponse.res.Data = oDecryptedData;
             }
-            aResponse.push(oResponse);    
+            aResponse.push(oResponse);
         }
         res.status(200).json({
             Message: aResponse,
         })
     } catch (error) {
-        debugger;
+        res.status(400).json({
+            Status: "Error",
+            Message: error,
+        })
+    }
+})
+
+app.post('/api/irn/fetch', async (req, res) => {
+    try {
+        let aData = req.body,
+        oHeader = req.headers,
+        aResponse = [],
+        oHeaders = {
+            "client_id": oHeader.client_id,
+            "client_secret": oHeader.client_secret,
+            "Gstin": oHeader.gstin,
+            "user_name": oHeader.user_name,
+            "AuthToken": oHeader.authtoken
+        };
+        
+    for (let i = 0; i < aData.length; i++) {
+        let oData = aData[i];
+        let sIrn = oData.irn_no,
+            oResponse = {};
+        oResponse.doc_no = oData.doc_no;
+        const { data } = await axios.get('gstcore/v1.02/Invoice/irn/' + sIrn, { headers: oHeaders });
+        oResponse.res = data;
+        if (!data.ErrorDetails) {
+            oDecryptedData = encrypt.aesDataDecryption(data.Data, oHeader.sek);
+            oResponse.res.Data = oDecryptedData;
+        }
+        aResponse.push(oResponse);
+    }
+        res.status(200).json({
+            Message: aResponse,
+        })
+    } catch (error) {
         res.status(400).json({
             Status: "Error",
             Message: error,
